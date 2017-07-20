3447ebf93ba9f7fb7cd809fa272b2b66da3d18c8
Tanweer Hossain <tanweer.hossain@juspay.in>
2017-07-20 16:19:03 +0530

    commit3

--- src/Main.purs
+++ src/Main.purs
@@ -2,15 +2,16 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff.Console (logShow,log)
-import Math (sqrt)
+import Math (sqrt,pi)
 import Data.List
 
 --main :: forall e. Eff (console :: CONSOLE | e) Unit
   --  add :: Int -> Int -> Int 
     --add x y = x + y 
+radius :: Number
+radius = 3.0
 main = do
-	log("Hello Tanweer")
-	logShow (_diagonal 3.0  4.0)
+	log("Area of circle with radius "<>(show radius)<>" is : "<>(show (circleArea radius)))
 
-_diagonal :: Number -> Number -> Number
-_diagonal b p = sqrt( b*b +p*p )
+circleArea :: Number -> Number
+circleArea r = (pi * r * r)
