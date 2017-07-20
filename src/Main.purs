621b7997d4f5d3f7fbbe51bdda818dd01bc0b547
Tanweer Hossain <tanweer.hossain@juspay.in>
2017-07-20 15:09:27 +0530

    commit2

--- src/Main.purs
+++ src/Main.purs
@@ -1,20 +1,16 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff.Console (log)
---import Data.List
+import Control.Monad.Eff.Console (logShow,log)
+import Math (sqrt)
+import Data.List
 
 --main :: forall e. Eff (console :: CONSOLE | e) Unit
   --  add :: Int -> Int -> Int 
     --add x y = x + y 
 main = do
-	log ("Hello xyz!..^_^ " <>(show hello))
+	log("Hello Tanweer")
+	logShow (_diagonal 3.0  4.0)
 
-hello :: Int
-hello = do
-	let x = 10
-	x
-
-
-iterate f 0 x = f x 
-iterate f n x = iterate f (n - 1) (f x)
+_diagonal :: Number -> Number -> Number
+_diagonal b p = sqrt( b*b +p*p )
