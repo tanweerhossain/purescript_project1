86784ad6d15ca88b6eef2c10879a6fc46970d8d6
Tanweer Hossain <tanweer.hossain@juspay.in>
2017-07-26 14:04:35 +0530

    commit11

--- src/Main.purs
+++ src/Main.purs
@@ -4,17 +4,33 @@ import Prelude
 import Control.Monad.Eff.Console (log)
 import Math (sqrt,pi)
 import Data.List
-import Data.AddressBook
+import Data.AddressBook 
 
 --main :: forall e. Eff (console :: CONSOLE | e) Unit
   --  add :: Int -> Int -> Int 
     --add x y = x + y 
 radius :: Number
 radius = 3.0
+
+circleArea :: Number -> Number
+circleArea r = (pi * r * r)
+
 main = do
 	log("Area of circle with radius "<>(show radius)<>" is : "<>(show (circleArea radius)))
 	log("Addition of "<>(show (radius+1.0))<>" and "<>(show radius)<>" is : "<>(show(_add (radius+1.0) radius)))
 	log("Sum of "<>(show (radius+1.0))<>" and "<>(show radius)<>" is : "<>(show(sum (radius+1.0) radius)))
-
-circleArea :: Number -> Number
-circleArea r = (pi * r * r)
+-- address :: Address
+-- address = {	
+-- 	street: "123 Fake St.",	
+-- 	city: "Faketown",	
+-- 	state: "CA"	
+-- }
+-- entry :: Entry
+-- entry = { 
+-- 	firstName: "John", 
+-- 	lastName: "Smith", 
+-- 	address: address 
+-- }
+-- book1 = insertEntry entry emptyBook
+-- printEntry "John" "Smith" book1
+	
