c134dfc048f0ae9d1675ad55b628874a205ad9a3
Tanweer Hossain <tanweer.hossain@juspay.in>
2017-07-27 19:36:43 +0530

    commit12

--- src/Data/AddressBook.purs
+++ src/Data/AddressBook.purs
@@ -30,12 +30,15 @@ sum = \x y -> x + y
 
 showEntry :: Entry -> String
 showEntry entry = do
-   entry.lastName <> ", " <> 
-   entry.firstName <> ": " <> 
+   entry.firstName <> " " <> 
+   entry.lastName <> ": " <> 
    showAddress entry.address
 printEntry :: String -> String -> AddressBook -> Maybe String
 printEntry firstName lastName book = map showEntry (findEntry firstName lastName book)
    
+printEntryByAddress :: Address -> AddressBook -> Maybe String
+printEntryByAddress address book = map showEntry $ findEntryByAddress address book
+
 showAddress :: Address -> String
 showAddress addr = do
     addr.street <> ", " <> 
@@ -48,9 +51,21 @@ emptyBook = empty
 insertEntry :: Entry -> AddressBook -> AddressBook
 insertEntry = Cons
 
+nubByEntry ::  AddressBook -> AddressBook
+nubByEntry book = nubBy (\a b ->  equateEntry a b) book
 
+equateEntry :: Entry -> Entry -> Boolean
+equateEntry a b = do
+  (a.firstName == b.firstName) && (a.lastName == b.lastName) 
+  
 findEntry :: String -> String -> AddressBook -> Maybe Entry
 findEntry firstName lastName book = head $ filter filterEntry book
   where
     filterEntry :: Entry -> Boolean
     filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+    
+findEntryByAddress :: Address -> AddressBook -> Maybe Entry
+findEntryByAddress address book = head $ filter filterEntry book
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.address.street == address.street && entry.address.city == address.city && entry.address.state == address.state
